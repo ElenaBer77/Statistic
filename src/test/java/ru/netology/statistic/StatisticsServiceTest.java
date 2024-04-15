cb6828d914ca6e0f521b0e7cda60d4a597ba15da
ElenaBer77 <pei281277@gmail.com>
2024-04-15 13:27:24 +0300

    everythingCovered

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,15 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-    //@Test
-    //void findMaxLast() {
-     //   StatisticsService service = new StatisticsService();
 
-      //  long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-      //  long expected = 12;
+    @Test
+    void findMaxLast() {
+        StatisticsService service = new StatisticsService();
 
-      //  long actual = service.findMax(incomesInBillions);
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-      //  Assertions.assertEquals(expected, actual);
-    //}
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
